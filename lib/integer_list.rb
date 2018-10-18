bb5b7b08339b2942f0f07fb3357322324a962e88
NLT <NamibiaLT@users.noreply.github.com>
2018-10-18 15:36:45 -0700

    remove non-interger function added

--- lib/integer_list.rb
+++ lib/integer_list.rb
@@ -1,5 +1,11 @@
 class IntegerList
   def self.duplicate_remover(array)
+    remove_non_integers(array)
     array.group_by(&:itself).keys
+  end
+
+  def self.remove_non_integers(array)
+    array.compact!
+    array.reject{|string| string.to_s}
   end
 end
--- spec/integer_list_spec.rb
+++ spec/integer_list_spec.rb
@@ -3,12 +3,16 @@ require 'rspec'
 
 RSpec.describe 'IntegerList' do
   let(:sample_array_1) { [1, 2, 3, 4, 5, 4] }
-  # let(:sample_array_2) { [3, nil, 8, 4, 3] }
-  # let(:sample_array_3) { [9, 'apple', 10, 7, 7, 8] }
+  let(:sample_array_2) { [3, nil, 8, 4, 3] }
+  let(:sample_array_3) { [9, 'apple', 10, 7, 7, 8] }
 
   describe 'duplicate_remover' do
     it 'removes duplcate integers from a list' do
       expect(IntegerList.duplicate_remover(sample_array_1)).to eq([1, 2, 3, 4, 5,])
+    end
+
+    it 'removes duplicates and nil values from the list'do
+      expect(IntegerList.duplicate_remover(sample_array_2)).to eq([3,8,4])
     end
   end
 end
