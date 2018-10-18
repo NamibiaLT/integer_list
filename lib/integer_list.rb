e7b97788b0c558516f00c45b0ec9a60b90ded292
NLT <NamibiaLT@users.noreply.github.com>
2018-10-18 15:43:37 -0700

    add explanation of uniq method

--- lib/integer_list.rb
+++ lib/integer_list.rb
@@ -2,7 +2,7 @@ class IntegerList
   def self.duplicate_remover(array)
     remove_non_integers(array)
     # array.group_by(&:itself).keys
-    array.uniq
+    array.uniq # ruby method that deletes duplicates
   end
 
   private
